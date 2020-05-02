3edc14cb82edb8e74179e22f38f18e1189e5858e
Bill <chen1223@bu.edu>
2020-05-02 16:05:57 +0800

    Finish in all layout

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  // Determine the current state of the hamburger menu
+  isActive = false;
+  productImgs = ['/assets/img/product1.jpg', '/assets/img/product2.jpg', '/assets/img/product3.jpg'];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu(): void {
+    this.isActive = !this.isActive;
+  }
+}
